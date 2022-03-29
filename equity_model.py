ea296ea8b8e61d71da0d1e677d4a590f421425e5
Axel Nilsson <Karlaxel.n@outlook.com>
2022-03-29 21:39:49 +0200

    Working GJR-GARCH(1,1) model. Tested on variey of assets.

--- equity_model.py
+++ equity_model.py
@@ -22,6 +22,8 @@ class StockData:
 
 
 class EquityModel:
+    _model_solution = None
+    _model_fitted = False
 
     def __init__(self, stock_data):
         self.stock_data = stock_data
@@ -37,52 +39,67 @@ class EquityModel:
             cons = self._likelihood_constraints_normal()
             func = self._likelihood_function_normal
 
-            omega0 = 0.01
+            # Initial conditions for GJR-GARCH(1,1) model.
+            omega0 = 0.001
             alpha0 = 0.05
-            beta0 = 0.90
+            beta0 = 0.80
+            beta1 = 0.02
             mu0 = np.mean(data)
-            x0 = [omega0, alpha0, beta0, mu0]
+            x0 = [omega0, alpha0, beta0, beta1, mu0]
 
-            garch_model_params = minimize(func, x0, constraints=cons, args=data)
+            garch_model_solution = minimize(func, x0, constraints=cons, args=data)
 
-            return garch_model_params
+            # Added to keep method non-static. Might be useful to keep in Equity Model object instead of re-running
+            # the optimization.
+            self._model_solution = garch_model_solution
+            self._model_fitted = True
+
+            return garch_model_solution
 
         elif model == 'generalized hyperbolic':
             cons = self._likelihood_constraints_generalized_hyperbolic()
             func = self._likelihood_constraints_generalized_hyperbolic
 
-            omega0 = 0.01
-            alpha0 = 0.05
-            beta0 = 0.90
-
-    def _likelihood_function_normal(self, params, data):
+    # noinspection PyMethodMayBeStatic
+    def _likelihood_function_normal(self, params, data) -> float:
         # param[0] is omega
         # param[1] is alpha
-        # param[2] is beta
-        # param[3] is µ
+        # param[2] is beta0
+        # param[3] is beta1 (asymmetry modifier)
+        # param[4] is mu
         # Note: vol_estimate is squared in this function.
+        # Note: The negative log likelihood value is returned to be minimized instead of maximized.
         n_observations = len(data)
         log_likelihood = 0
-        initial_squared_vol_estimate = params[0] + params[1] * (data[0] ** 2) + params[2] * (data[0] ** 2)
+        initial_squared_vol_estimate = (params[0]
+                                        + params[1] * (data[0] ** 2)
+                                        + params[3] * (data[0] ** 2) * np.where(data[0] < 0, 1, 0)
+                                        + params[2] * (data[0] ** 2))
         current_squared_vol_estimate = initial_squared_vol_estimate
 
         for i in range(1, n_observations):
             log_likelihood = (log_likelihood
-                              + ((data[i-1] - params[3]) ** 2) / current_squared_vol_estimate
+                              + ((data[i-1] - params[4]) ** 2) / current_squared_vol_estimate
                               + 2 * np.log(np.sqrt(current_squared_vol_estimate)))
 
             current_squared_vol_estimate = (params[0] + params[1] * (data[i-1] ** 2)
+                                            + params[3] * (data[i-1] ** 2) * np.where(data[i-1] < 0, 1, 0)
                                             + params[2] * current_squared_vol_estimate)
 
         return log_likelihood
 
-    def _likelihood_constraints_normal(self):
-        cons_garch = [{'type': 'ineq', 'fun': lambda x: -x[1] - x[2] + 1},
+    # noinspection PyMethodMayBeStatic
+    def _likelihood_constraints_normal(self) -> dict:
+        # x[0] is omega
+        # x[1] is alpha
+        # x[2] is beta0
+        # x[3] is beta1 (asymmetry modifier)
+
+        cons_garch = [{'type': 'ineq', 'fun': lambda x: -x[1] - x[2] - (0.5 * x[3]) + 1},
                       {'type': 'ineq', 'fun': lambda x:  x[0]},
-                      {'type': 'ineq', 'fun': lambda x:  x[1]},
+                      {'type': 'ineq', 'fun': lambda x:  x[1] + x[3]},
                       {'type': 'ineq', 'fun': lambda x:  x[2]}]
         return cons_garch
-
 
     def _likelihood_function_generalized_hyperbolic(self, params, data):
         pass
--- main.py
+++ main.py
@@ -6,7 +6,13 @@ from matplotlib import pyplot as plt
 
 
 if __name__ == '__main__':
-    assets_ric = ['GS', 'XOM', '^GSPC', 'AUDUSD=X', 'EURUSD=X', 'WFC']
+    # Testing sensibility of GJR-GARCH modelling by assets in different asset classes.
+    # Note: Testing assets include non-equity class assets.
+    assets_ric = ['^GSPC', '^IXIC', '^RUT', '^N225',
+                  'GS', 'XOM', 'WFC', 'MS', 'KO', 'T', 'VZ',
+                  'AUDUSD=X', 'EURUSD=X', 'JPYUSD=X', 'NOKUSD=X', 'GBPUSD=X',
+                  'CL=F', 'GC=F', 'SI=F',
+                  '^TNX']
     assets = [StockData(ric) for ric in assets_ric]
     for asset in assets:
         parse_yahoo_data(asset)
@@ -17,20 +23,29 @@ if __name__ == '__main__':
         omg = params[0]
         alp = params[1]
         bet = params[2]
+        gam = params[3]
 
         time = data.index[1:]
         data = data.to_list()
         vol = []
-        curr_vol = omg + alp * (data[0] ** 2) + bet * (data[0] ** 2)
+        curr_vol = (omg
+                    + alp * (data[0] ** 2)
+                    + gam * (data[0] ** 2) * np.where(data[0], 1, 0)
+                    + bet * (data[0] ** 2))
         vol.append(curr_vol)
 
         for i in range(2, len(data)):
-            curr_vol = omg + alp * (data[i-1] ** 2) + bet * (vol[i-2])
+            curr_vol = (omg
+                        + alp * (data[i-1] ** 2)
+                        + gam * (data[i-1] ** 2) * np.where(data[i-1] < 0, 1, 0)
+                        + bet * (vol[i-2]))
             vol.append(curr_vol)
 
         vol = np.sqrt(vol)
-        plt.plot(time[15:], vol[15:])
+        plt.plot(time[20:], vol[20:])
         plt.show()
 
     for eq in equity_models:
+        plt.title(eq.stock_data.ric)
         plot_vol(eq.stock_data.get_log_returns(), eq.fit_model().x)
+
