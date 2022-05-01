3d2805eecac9afb2a3cca7de88ec7015058917a7
Axel Nilsson <Karlaxel.n@outlook.com>
2022-05-02 01:15:38 +0200

    Working connection to db, with working queries.

--- database/database_parser.py
+++ database/database_parser.py
@@ -1,3 +1,37 @@
 """
 TODO: Implement data parser from Refinitiv Eikon to abacus_database.db automatically.
 """
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file: str) -> object:
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        connection = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    return connection
+
+
+def select_all_tasks(connection: object) -> list:
+    """
+    Query all rows in the tasks table
+    :param connection: the Connection object
+    :return:
+    """
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM asset_model_student_init_solutions")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row[4])
+
+
+conn = create_connection('abacus_database.db')
+select_all_tasks(conn)
